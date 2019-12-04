afc2fb89fe484b4ee9d8b348fb78fa11e4e1247a
İbrahim Şakar <ibrahims.sakar@setur.com.tr>
2019-12-04 21:16:42 +0300

    Example keys added.

--- etc/config.production.js
+++ etc/config.production.js
@@ -5,6 +5,10 @@ const config = {
         version: '1.0.0',
         description: '',
     },
+    jwt: {
+        private: './etc/private.key',
+        public: './etc/public.key',
+    },
 };
 
 module.exports = config;
